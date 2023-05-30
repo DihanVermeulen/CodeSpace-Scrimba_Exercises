e8d21a40541738d9cdf518a347cbc524a4cc2e98
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-30 16:35:19 +0200

    Updated

--- tenzies/src/components/Layout.tsx
+++ tenzies/src/components/Layout.tsx
@@ -1,20 +1,24 @@
 import { ReactElement } from "react";
 
 type Props = {
-  children?: ReactElement;
+  children?: ReactElement | ReactElement[];
 };
 
 const Layout = (props: Props) => {
   const { children } = props;
   return (
     <>
-      <header className="header">
-        <h1 className="header__heading">Tenzies</h1>
-        <p className="header__description">
-          Roll until all dice are the same. Click a dice to freeze it.
-        </p>
-      </header>
-      <section>{children}</section>
+      <div style={{ padding: "2rem" }}>
+        <header className="header">
+          <h1 className="header__heading">Tenzies</h1>
+          <p className="header__description">
+            Roll until all dice are the same. Click a dice to freeze it in
+            place.
+          </p>
+        </header>
+        <div className="space" />
+        <section>{children}</section>
+      </div>
     </>
   );
 };
