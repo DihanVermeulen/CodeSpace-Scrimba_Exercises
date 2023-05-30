f62000ffabc9856aab28a3c974997df7526bd627
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-30 16:34:28 +0200

    Added basic structure to app

--- tenzies/src/App.tsx
+++ tenzies/src/App.tsx
@@ -4,7 +4,18 @@ import Layout from "./components/Layout";
 function App() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <main className="main__section"></main>
+        <div className="space" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <button className="roll__button">Roll</button>
+        </div>
+      </Layout>
     </>
   );
 }
