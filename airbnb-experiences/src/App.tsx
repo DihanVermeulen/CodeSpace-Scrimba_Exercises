5b5eb4d8747a3d314d675bfb1af65e4f6a883e10
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-01 17:44:16 +0200

    Added home layout inside of navigation layout

--- airbnb-experiences/src/App.tsx
+++ airbnb-experiences/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import { HomeLayout } from "./components/layout/HomeLayout";
 import { NavigationLayout } from "./components/layout/NavigationLayout";
 
 function App() {
   return (
     <>
       <NavigationLayout>
-        
+        <HomeLayout />
       </NavigationLayout>
     </>
   );
