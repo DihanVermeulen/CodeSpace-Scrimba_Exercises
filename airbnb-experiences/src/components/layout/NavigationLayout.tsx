33913a8f463582d3e9d97b997a522760af0f7d02
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-01 14:25:39 +0200

    Created a navigation layout

---
+++ airbnb-experiences/src/components/layout/NavigationLayout.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from "react";
+import { Header } from "../Header";
+
+type Props = {
+  children: ReactElement | ReactElement[];
+};
+
+export const NavigationLayout = (props: Props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
