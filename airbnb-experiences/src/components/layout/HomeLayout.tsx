b0f6f238aaa9249a3585fb14cbad926accc9d4c1
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-01 17:43:46 +0200

    Added hero section as well as title to home layout

--- airbnb-experiences/src/components/layout/HomeLayout.tsx
+++ airbnb-experiences/src/components/layout/HomeLayout.tsx
@@ -1,10 +1,56 @@
-import { ReactElement } from "react";
+import styled from "styled-components";
+import heroSectionImageSmall from "../../assets/hero-section-small.png";
+import heroSectionImageLarge from "../../assets/hero-section-large.png";
 
-type Props = {
-  children: ReactElement | ReactElement[];
+const Wrapper = styled.section`
+  padding: 2rem;
+`;
+
+const CenterContentSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Space = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const styles = {
+  centerContent: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  description: {
+    fontWeight: 300,
+    fontSize: "16px",
+  },
+  heading: {
+    fontWeight: 600,
+    fontSize: "32px",
+  },
 };
 
-export const HomeLayout = (props: Props) => {
-  const { children } = props;
-  return <>{children}</>;
+export const HomeLayout = () => {
+  return (
+    <Wrapper>
+      <CenterContentSection>
+        <picture style={styles.centerContent}>
+          <source media="(min-width:650px)" srcSet={heroSectionImageLarge} />
+          <img
+            src={heroSectionImageSmall}
+            alt="Flowers"
+            style={{ width: "90%" }}
+          />
+        </picture>
+      </CenterContentSection>
+      <Space />
+      <h1 style={styles.heading}>Online Experiences</h1>
+      <p style={styles.description}>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </Wrapper>
+  );
 };
