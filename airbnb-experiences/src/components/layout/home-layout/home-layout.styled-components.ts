ed0cfe1d3afa9dae471a6dbab2d6011305b1c136
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-06-02 10:20:17 +0200

    Created styled components file to abstract away from component file

---
+++ airbnb-experiences/src/components/layout/home-layout/home-layout.styled-components.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  overflow-x: hidden;
+  width: 100%;
+`;
+
+export const CenterContentSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+`;
